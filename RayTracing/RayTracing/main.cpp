5b24f4365362854a1117c44674f5f018d3cb3a52
zhangci <zhangci226@163.com>
2017-11-29 15:48:28 +0800

    updated

--- RayTracing/RayTracing/main.cpp
+++ RayTracing/RayTracing/main.cpp
@@ -1,18 +1,38 @@
+#include <opencv2/opencv.hpp>
 #include "Tracer.h"
+
+#define IMAGE_WIDTH 800
+#define IMAGE_HEIGHT 600
 
 int main(void)
 {
-	CScene *scene = new CScene();
 
-	CDirectionalLight *DL1 = new CDirectionalLight(GVector3(), GVector3(.0f, .0f, 1.0f));
-	CPlane *Pi1 = new CPlane();
+	CvSize csize;
+	csize.width = IMAGE_WIDTH;
+	csize.height = IMAGE_HEIGHT;
 
-	scene->AddLight(DL1);
-	scene->AddObject(Pi1);
+	IplImage *pImg = cvCreateImage(csize, 8, 3);
 
-	CRay r;
+	CvScalar cs;
+	for (int i = 0; i < IMAGE_HEIGHT; i++)
+	{
+		for (int j = 0; j < IMAGE_WIDTH; j++)
+		{
+			cs = cvGet2D(pImg, i, j);
+			cs.val[2] = 255;
+			cs.val[1] = 255;
+			cs.val[0] = 255;
+			cvSet2D(pImg, i, j, cs);
+		}
+	}
 
-	CTracer::Trace(scene, r, 1);
+	cvShowImage("RayTracing", pImg);
+
+	cvSaveImage("RayTracing.bmp", pImg);
+	cvWaitKey();
+
+	cvReleaseImage(&pImg);
+
 	return 1;
 
 }
